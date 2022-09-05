c6f95b017fbd41fcb32258ea4a9686344fc0c191
Italo Amaral <italorockamaral@gmail.com>
2022-09-04 21:30:28 -0300

    FIX: Fixed some bugs in tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,16 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-  ],
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+       colors: {
+         brand: {
+           primary: "#A0EDF0",
+           secondary: "#F56F36",
+           dark: "#000",
+           lightDark: "#1F2738",
+         }
+       },
+    },
   },
   plugins: [],
 }
